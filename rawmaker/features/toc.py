3f4b5f67d93338cd35d8b9be23f1366f5b82ba4c
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-04-12 19:33:29 +0200

    refactor(create_toc) move create_toc to iamraw

--- rawmaker/features/toc.py
+++ rawmaker/features/toc.py
@@ -11,69 +11,11 @@
 Basic structure of get_outlines: (level, title, args, children)
 """
 
-from typing import List
-
 from iamraw import Section
-from iamraw import Toc
+from iamraw import create_toc
 from pdfminer.pdfdocument import PDFDocument
 from serializeraw.toc import dump_yaml as dump_toc
 from utila import Command
-
-
-def parse_toc(outlines: List[Section]):
-    """Extract toc out of pdf-outlines
-
-    The highest level is 0 the document root. Higher number level means more
-    distance to root.
-    """
-    root = Toc()
-    current = root
-    for item in outlines:
-        level = item.level
-        if level == current.level:
-            # Content is on the same level, therefore they have the same
-            # parent together.
-            new_one = Section(
-                parent=current.parent,
-                level=item.level,
-                title=item.title,
-            )
-            add_children(current.parent, new_one)
-        elif level > current.level:
-            # The level of the item to add is higher than the current item in
-            # table of content, therefore add the new one as a paranet of
-            # current.
-            new_one = Section(
-                parent=current,
-                level=item.level,
-                title=item.title,
-            )
-            add_children(current, new_one)
-        else:
-            # The level of the `new_one` is lower than the item in index. That
-            # means that the distance of the item to add to the index is
-            # samller as the current one.
-            # For example: Current = 1.4.4.2
-            #              item    = 1.5
-            # We have to go up in the tree to find a common parent of both
-            # and add item.
-            while level <= current.level:
-                if current.parent:
-                    current = current.parent
-                    continue
-            new_one = Section(
-                parent=current,
-                level=item.level,
-                title=item.title,
-            )
-            add_children(current, new_one)
-        current = new_one
-    return root
-
-
-def add_children(section: Section, item):
-    assert section, item
-    section.children.append(item)
 
 
 def commandline():
@@ -85,7 +27,7 @@ def work(document: PDFDocument):
 
     data = [Section(level, title) for (level, title, dest, a, se) in outlines]
 
-    toc = parse_toc(data)
+    toc = create_toc(data)
 
     # toc to yaml
     yaml = dump_toc(toc)

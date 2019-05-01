7104687b239d29c7a9418a072ff92de2800241b5
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-05-01 17:12:25 +0200

    refactor(features): remove unused code

--- rawmaker/features/__init__.py
+++ rawmaker/features/__init__.py
@@ -24,41 +24,25 @@ from utila import Command
 from utila import logging
 from utila import logging_error
 
-import rawmaker.features
 from rawmaker import ROOT
 
-LOADED = False
+FEATURE_PATH_PACKAGE = 'rawmaker.features'
+REQUIRED_METHODS = {'commandline', 'work'}
 
 
 def find_features(path: str):
     """Locate all feautures in given path
-
-    Args:
-        path(str): absoulte path to feature location
-    Retuns:
-        list of python files, which are possible features, as relative path to
-        `path`
     """
-    msg = 'Path does not exists %s' % path
-    assert exists(path), msg
-
-    result = [
-        item for item in listdir(path)
+    assert exists(path), path
+    collected = [
+        item.replace('.py', '')
+        for item in listdir(path)
         if not '__init__' in item and item.endswith('.py')
     ]
-
-    result = [item.replace('.py', '') for item in result]
-
-    return result
-
-
-def features(path: str):
-    features = find_features(path)
     result = []
-    for item in features:
+    for item in collected:
         current = import_module(FEATURE_PATH_PACKAGE + '.' + item,
                                 FEATURE_PATH_PACKAGE)
-
         try:
             result.append((current.name(), current.commandline, current.work))
         except AttributeError as exception:
@@ -76,59 +60,3 @@ def commandline(features):
         result.append(command())
 
     return result
-
-
-def load_features(feature_root: str, features: List[str]):
-    """Load list with possible features
-
-    Args:
-        feature_root(str): absolute path to feature location
-        features(List[str]): relative path to features
-    Returns:
-        SUCCESS if every possible feautre is loaded successfull else FAILURE
-    """
-    ret = 0
-    for item in features:
-        path = join(feature_root, item + '.py')
-        ret += load_module(path, FEATURE_PATH_PACKAGE)
-
-    return ret
-
-
-FEATURE_PATH_PACKAGE = 'rawmaker.features'
-REQUIRED_METHODS = {'commandline', 'work'}
-
-
-def load_module(path: str, sub_module: str):
-    """Load feature and check the interface
-
-    Args:
-        path(str): absoulte path to feature
-        sub_module(str): packagename of the featurelocation, see
-                         FEATURE_PATH_PACKAGE
-    Returns:
-        SUCCESS if the feature provide all REQUIRED_METHODS else FAILURE
-    """
-    module_name = sub_module + '.' + basename(path)
-
-    # load module from path
-    spec = spec_from_file_location(module_name, path)
-    module = module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    # check the provision of all `REQUIRED_METHODS`
-    methods = set(dir(module))
-    if len(REQUIRED_METHODS & methods) == len(REQUIRED_METHODS):
-        if not module_name in modules.keys():
-            modules[module_name] = module
-            logging('LOAD FEATURE: %s' % module.__name__)
-        else:
-            logging('SKIP LOADING: %s' % module.__name__)
-        return SUCCESS
-    else:
-        missing_methods = REQUIRED_METHODS - methods
-
-        for item in missing_methods:
-            logging_error('MISSING METHOD %s in %s' % (item, module.__name__))
-        logging_error('ABORT LOADING')
-        return FAILURE

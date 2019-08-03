d7f30d2ed483da8c9ffbee8735ab88e272ffa673
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-08-03 20:02:41 +0200

    1.1.9 automatically generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v1.1.9
+
 ## v1.1.8
 
 ## v1.1.7
--- rawmaker/__init__.py
+++ rawmaker/__init__.py
@@ -13,7 +13,7 @@ Hint: Pay attention to the public API on this file!
 """
 import os
 
-__version__ = '1.1.8'
+__version__ = '1.1.9'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 

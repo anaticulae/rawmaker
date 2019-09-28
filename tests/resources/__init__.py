4870790164be52a6bd344168cfa44dd3fd06a695
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-09-28 13:27:34 +0200

    refactor(resources): cleanup test resource provider

--- tests/resources/__init__.py
+++ tests/resources/__init__.py
@@ -13,10 +13,14 @@ from os.path import join
 
 from rawmaker import ROOT
 
-RESOURCES = join(ROOT, 'tests/resources')
+RESOURCES = os.path.join(ROOT, 'tests/resources')
 assert exists(RESOURCES), RESOURCES
 
-NEGATIVE = join(RESOURCES, 'negative')
+BACHELOR = os.path.join(RESOURCES, 'bachelor')
+MASTER = os.path.join(RESOURCES, 'master')
+NEGATIVE = os.path.join(RESOURCES, 'negative')
+HELLO_WORLD = join(RESOURCES, 'helloworld')
+TECHNICAL = os.path.join(RESOURCES, 'technical')
 
 NO_PDF = join(NEGATIVE, 'no_pdf')
 
@@ -24,12 +28,11 @@ VIM_GUIDE_PDF = join(RESOURCES, 'vimguide.pdf')
 VIM_GUIDE_PAGE_COUNT = 13
 VIM_GUIDE_OUTLINES = 42
 
-HELLO_WORLD = join(RESOURCES, 'helloworld')
 HELLO_WORLD_PDF = join(HELLO_WORLD, 'hello_world.pdf')
 HELLO_WORLD_PAGES = 1
 HELLO_WORLD_OUTLINES = 0
 
-DOCUMENTATION_TWINE = join(RESOURCES, 'technical/twine')
+DOCUMENTATION_TWINE = join(TECHNICAL, 'twine')
 DOCUMENTATION_TWINE_PDF = join(DOCUMENTATION_TWINE, 'documentation_twine.pdf')
 DOCUMENTATION_TWINE_PAGES = 35
 
@@ -38,28 +41,11 @@ RESTRUCTURED_PDF = TOC_PDF
 
 PORTING_PYTHON3 = os.path.join(RESOURCES, 'broken/broken_annotation/porting_python3.pdf') # yapf:disable
 
-for item in [
-        DOCUMENTATION_TWINE_PDF,
-        HELLO_WORLD_PDF,
-        NO_PDF,
-        PORTING_PYTHON3,
-        TOC_PDF,
-        VIM_GUIDE_PDF,
-]:
-    assert exists(item), item
 
 INCREASING_FONT = join(RESOURCES, 'increasing_fonts')
 INCREASING_FONT_A3 = join(INCREASING_FONT, 'increasing_fonts_a3.pdf')
 INCREASING_FONT_A4 = join(INCREASING_FONT, 'increasing_fonts_a4.pdf')
 INCREASING_FONT_A5 = join(INCREASING_FONT, 'increasing_fonts_a5.pdf')
-
-for item in [
-        INCREASING_FONT,
-        INCREASING_FONT_A3,
-        INCREASING_FONT_A4,
-        INCREASING_FONT_A5,
-]:
-    assert exists(item), item
 
 HOW_TO_CPORTING = join(RESOURCES, 'boxes')
 HOW_TO_CPORTING_PDF = join(HOW_TO_CPORTING, 'howto_cporting.pdf')
@@ -69,5 +55,26 @@ assert exists(HOW_TO_CPORTING_PDF), HOW_TO_CPORTING_PDF
 HOW_TO_CPORTING_BOX_COUNT = 10
 HOW_TO_CPORTING_HORIZONTAL_COUNT = 2
 
-MASTER_72_NOIMAGES_TOC = os.path.join(RESOURCES, 'master/page_72_noimages_toc.pdf') # yapf:disable
+MASTER_72_NOIMAGES_TOC = os.path.join(MASTER, 'page_72_noimages_toc.pdf')
 assert os.path.exists(MASTER_72_NOIMAGES_TOC), str(MASTER_72_NOIMAGES_TOC)
+
+REQUIRED_RESOURCES = [
+    BACHELOR,
+    DOCUMENTATION_TWINE_PDF,
+    HELLO_WORLD_PDF,
+    INCREASING_FONT,
+    INCREASING_FONT_A3,
+    INCREASING_FONT_A4,
+    INCREASING_FONT_A5,
+    MASTER_72_NOIMAGES_TOC,
+    NEGATIVE,
+    NO_PDF,
+    PORTING_PYTHON3,
+    RESOURCES,
+    TECHNICAL,
+    TOC_PDF,
+    VIM_GUIDE_PDF,
+]
+
+MISSING = [item for item in REQUIRED_RESOURCES if not os.path.exists(item)]
+assert not MISSING, f'missing: {MISSING}'

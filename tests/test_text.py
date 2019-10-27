6ca5a8ad9aa425ee7c2d183e82c4b4dbb4aa74b9
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-10-27 17:32:57 +0100

    fix(test): compare extracted test before saving
    In the old case, the dumped and loaded content is compared. The problem
    of this test case is that, if there are problems by the first dump we
    can not detect these problem because the loaded document later does not
    represent the origin document.

--- tests/test_text.py
+++ tests/test_text.py
@@ -6,10 +6,13 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
+import iamraw
+import utila
 from pytest import mark
 from serializeraw import dump_document
 from serializeraw import load_document
 
+import rawmaker.features.text
 from rawmaker.features.text import work
 from tests.resources import HELLO_WORLD_PAGES
 from tests.resources import HELLO_WORLD_PDF
@@ -30,25 +33,40 @@ def test_mine_hello_world_pdf():
 
 
 @mark.parametrize(
-    'pdf_resource', [
+    'pdf_path',
+    [
         HELLO_WORLD_PDF,
         VIM_GUIDE_PDF,
     ],
     ids=[
         'hello_world.pdf',
         'vimguide.pdf',
-    ])
-def test_dump_and_load_pdf(pdf_resource):
+    ],
+)
+def test_dump_and_load_pdf(pdf_path):
     """Parse text from pdf file and write the result. Load the result after
     and compare with item to save"""
 
-    text, _ = work(pdf_resource)
+    text = rawmaker.features.text.extract_document(pdf_path)
     assert text
 
-    loaded_yaml = load_document(text)
-    another_dump = dump_document(loaded_yaml)
+    dumped = dump_document(text)
 
-    assert another_dump == text
+    loaded = load_document(dumped)
+
+    # Saving document saves only data not the bounding. The bounding is
+    # stored in an other class. Therefore we have to ensure that
+    # BoudingBox'es are equal before we compare the content.
+    # NOTE: IN THIS CASE WE COMPARE ONLY THE TEXT CONTENT
+    assert len(loaded) == len(text)
+    for pageloaded, pageexpected in zip(loaded, text):
+        for itemloaded, itemexpected in zip(pageloaded, pageexpected):
+            # itemloaded.box = itemexpected.box
+            if not isinstance(itemloaded, iamraw.TextContainer):
+                continue
+
+            for line, lineexpe in zip(itemloaded.lines, itemexpected.lines):
+                assert line.text == lineexpe.text
 
 
 def test_text_mine_pdf_page_0():

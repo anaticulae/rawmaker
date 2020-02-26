0237b295f920e77865413c69890b2a70f82df140
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-02-26 13:02:32 +0100

    test(regression): add test with failing font parser

--- tests/fonts/test_regression.py
+++ tests/fonts/test_regression.py
@@ -38,3 +38,21 @@ def test_leftright_book_font_size(testdir, monkeypatch):
     # ensure that a higher font size than 1 is detected. There was/is a
     # bug with font size determination.
     assert first_char.size >= 8.0, first_char
+
+
+# @pytest.mark.xfail(reason='font parsing error')
+def test_porting_module_font_index(testdir, monkeypatch):
+    tests.run_success(
+        f'-i {tests.resources.HOW_TO_CPORTING_PDF} --fonts --pages=0',
+        monkeypatch=monkeypatch,
+    )
+    source = rawmaker.path.fontcontent(testdir.tmpdir)
+    position = serializeraw.load_font_content(source)
+    first_page = utila.select_page(position, page=0)[0]
+
+    before_last = first_page[-2][0:3]
+    last = first_page[-1][0:3]
+
+    assert before_last != last, ('font extraction error, 2 different '
+                                 'location can not be equal'
+                                 f'{before_last} {last}')

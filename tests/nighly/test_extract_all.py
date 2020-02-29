a35a3d98a01484a1cb25c7e125e4c3bbe65473eb
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-02-29 21:17:29 +0100

    test(nightly): add nightly tests to run rawmaker, linero, and pdfinfo
    with all existing pdf files.
    
    Solves release plan goal

---
+++ tests/nighly/test_extract_all.py
@@ -0,0 +1,56 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+"""Nighly: Extract All PDF Files
+=============================
+
+This long running nightly test must ensure that all pdf files in
+reposity are extractable by rawmaker and exit with exitcode 0.
+"""
+
+import glob
+import os
+
+import pytest
+import utila
+
+import tests.resources
+
+
+def sources():
+    root = tests.resources.RESOURCES
+    with utila.chdir(root):
+        collected = glob.glob('**/*.pdf')
+    joined = [utila.make_absolute(item, cwd=root) for item in collected]
+    result = [
+        pytest.param(
+            item,
+            id=utila.make_relative(item, root=root),
+        ) for item in joined
+    ]
+    return result
+
+
+@utila.skip_nightly
+@pytest.mark.parametrize('source', sources())
+def test_nightly_extract_all_rawmaker_linero(source, testdir):  # pylint:disable=W0621
+    """Collect all existing pdf files an extract all features out of it."""
+    source = os.path.join(tests.resources.RESOURCES, source)
+
+    completed = utila.run(f'rawmaker -i {source} -j=8', cwd=testdir.tmpdir)
+    assert completed.returncode == utila.SUCCESS, str(completed)
+
+    completed = utila.run(f'linero -j=8', cwd=testdir.tmpdir)
+    assert completed.returncode == utila.SUCCESS, str(completed)
+
+
+@utila.skip_nightly
+@pytest.mark.parametrize('source', sources())
+def test_nightly_pdfinfo(source, testdir):  # pylint:disable=W0621
+    completed = utila.run(f'pdfinfo -i {source}', cwd=testdir.tmpdir)
+    assert completed.returncode == utila.SUCCESS, str(completed)

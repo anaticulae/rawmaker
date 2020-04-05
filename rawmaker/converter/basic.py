9496445541b987172c5981abe14e72254ce69ffe
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-04-05 09:42:39 +0200

    fix(converter): ensure that malformed bounding let not fail extraction

--- rawmaker/converter/basic.py
+++ rawmaker/converter/basic.py
@@ -20,17 +20,21 @@ class FlippedLayoutAnalyzer(pdfminer.converter.PDFLayoutAnalyzer):
         super().__init__(rsrcmgr=rsrcmgr, pageno=pageno, laparams=laparams)
 
     def receive_layout(self, ltpage):
-        pageheight = ltpage.height
         for item in ltpage:
-            flip_object(item, pageheight)
+            flip_object(item, ltpage)
 
 
-def flip_object(item, pageheight):
+def flip_object(item, page):
+    pageheight = page.height
     box = list(item.bbox)
     box[1], box[3] = pageheight - box[3], pageheight - box[1]
     box = utila.roundme(box)  # pylint:disable=R0204
-    item.bbox = iamraw.BoundingBox(*box)
+    try:
+        item.bbox = iamraw.BoundingBox(*box)
+    except AssertionError:
+        utila.error(f'invalid bounding on page {page}: {box}')
+        utila.error(item)
     item.x0, item.y0, item.x1, item.y1 = box
     with contextlib.suppress(AttributeError):
         for obj in item._objs:  # pylint:disable=W0212
-            flip_object(obj, pageheight)
+            flip_object(obj, page)

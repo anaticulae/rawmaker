9d56190a077b215e050fb0c957b9510b6f871b49
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-04-11 19:38:44 +0200

    test(text): ensure that text is not parsed twice

---
+++ tests/layout/test_text.py
@@ -0,0 +1,30 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2020 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import serializeraw
+import utila
+
+import tests
+import tests.resources
+
+
+def test_bachelor63_text_extraction(testdir, monkeypatch):
+    """This is a regression test, which ensure that a `pdfminer` bug
+    does no occurs again. There was a problem that some text was parsed
+    twice and more."""
+    root = testdir.tmpdir
+    source = tests.resources.BACHELOR63
+    config = ''
+    cmd = f'-i {source} --text --pages=0 {config}'
+    tests.run_success(cmd, monkeypatch=monkeypatch)
+
+    navigator = serializeraw.create_pagetextnavigators_frompath(root)[0]
+    text = [str(item).strip() for item in navigator]
+    unique = utila.make_unique(text)
+    assert len(unique) == len(text), str(text)

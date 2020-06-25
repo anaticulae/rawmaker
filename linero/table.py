a826b3e657f274ffe5c476f3de31b81177f73009
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-25 16:10:22 +0200

    feat(table): add table validator

--- linero/table.py
+++ linero/table.py
@@ -7,12 +7,38 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import utila
+
 TABLE_MIN_HEIGHT = 50  # TODO: HOLY VALUE
+MAX_SINGLE_LINE_QUOTE = 0.2
 
 
 def valid_table(bounding, navigator) -> bool:
-    height = bounding[3] - bounding[1]
+    top, bottom = bounding[1], bounding[3]
+
+    height = bottom - top
     if height < TABLE_MIN_HEIGHT:
         # remove to small tables
         return False
+
+    table_content = navigator.between(
+        top / navigator.height,
+        bottom / navigator.height,
+    )
+    if not table_content:
+        # no content in table
+        return False
+
+    boundings = [item.bounding for item in table_content]
+    clustered = utila.same_line_cluster(
+        boundings,
+        min_elements=1,
+    )
+    singles = len([item for item in clustered if len(item) == 1])
+    single_quote = singles / len(clustered)
+    if singles >= 2 and single_quote > MAX_SINGLE_LINE_QUOTE:
+        # invalid table content
+        return False
+
+    # table seems to be valid
     return True

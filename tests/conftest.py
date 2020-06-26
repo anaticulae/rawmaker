801c2c3f473450f17e1cd604996fa14f11cc6e49
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-26 22:54:01 +0200

    test(data); extend test data generator

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,6 +28,7 @@ def pytest_sessionstart():
 
 def extract():
     todo = [
+        (power.MASTER072_PDF, power.link(power.MASTER072_PDF), None),
         (power.BACHELOR056_PDF, power.link(power.BACHELOR056_PDF), '14:34'),
         (power.BOOK007_PDF, power.link(power.BOOK007_PDF), None),
         (power.DOCU13_PDF, power.link(power.DOCU13_PDF), None),

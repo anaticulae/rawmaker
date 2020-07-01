c7e879a6e78adf7c4b61edf0e67a6fbbfe9edbe6
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-07-01 18:22:01 +0200

    2.1.1 automatically generated release

--- rawmaker/__init__.py
+++ rawmaker/__init__.py
@@ -13,7 +13,7 @@ Hint: Pay attention to the public API on this file!
 """
 import os
 
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 

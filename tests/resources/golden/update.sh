8816a71dec023f2afdc5bd59a243fc7eedcd17fc
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-08-10 15:27:11 +0200

    chore(fix): adjust golden master test generator

--- tests/resources/golden/update.sh
+++ tests/resources/golden/update.sh
@@ -7,8 +7,10 @@ echo "update golden master"
 
 pushd $scriptroot
 
-todo="--images!"
-rawmaker -i ./../../resources/docu/vimguide.pdf -j8 --pages=1:4 ${todo} -o vim
+todo="--images! --figures!"
+
+vimguide=$(python -c 'import power; print(power.DOCU13_PDF)')
+rawmaker -i ${vimguide} -j8 --pages=1:4 ${todo} -o vim
 
 # TODO COMPARE $0 with SUCCESS
 echo "done"

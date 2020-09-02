b6d435281e0f5c6aa4c4281d6e13ea05e6b36261
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-09-02 15:54:26 +0200

    test(formula): add test to verify multiple lines formula

--- tests/formula/test_master116.py
+++ tests/formula/test_master116.py
@@ -8,6 +8,7 @@
 # =============================================================================
 
 import power
+import pytest
 import utila
 
 import rawmaker.math
@@ -29,6 +30,23 @@ def test_extract_math_master116_page22():
         '𝐹𝐺=𝑚𝑣·𝑔,(2.1)',
     ]
     assert raw == expected
+
+
+@pytest.mark.xfail(reason='support multiple formulas')
+def test_extract_math_master116_page23_multiple_lines():
+    source = power.MASTER116_PDF
+    with rawmaker.reader.read(source) as pdf:
+        extracted = rawmaker.math.extract_content(pdf, pages=(23,))
+    assert extracted
+    content = utila.select_content(extracted, page=23)
+    raw = raw_formula(content)
+    expected = [
+        '𝐹𝑊=𝐹𝐿+𝐹𝑅+𝐹𝑆𝑡+𝐹𝐴,(2.5)',
+        '𝐹𝐿=0.5𝜌𝐿𝑐𝑤𝐴𝑣𝑣𝑟2,(2.6)',
+        '𝜌𝐿=1,2041kg/m3.(2.7)',
+        '𝑐𝑤(𝑛)={1,0 n=0, 1,3 n=1, 1,53+(𝑛−2)·0,14} 𝑛≥2.(2.8)',
+    ]
+    assert len(raw) == len(expected)
 
 
 def raw_formula(formulas) -> list:

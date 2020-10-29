2f651d959e5361d0739b81be2db480e61261413e
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-10-29 18:04:52 +0100

    patch(utila): add cluster code to merge rectangles

--- rawmaker/__patch__.py
+++ rawmaker/__patch__.py
@@ -50,3 +50,61 @@ def intersecting_ending(first: tuple, second: tuple, tol: float = 3.0) -> bool:
 
 
 utila.intersecting_ending = intersecting_ending
+
+
+def intersecting_rectangle_cluster(
+        todo,
+        min_elements: int = 1,
+        bounding: callable = None,
+):
+    if not bounding:
+        bounding = lambda item: item
+
+    def classifier(candidat, clusteritem):
+        return intersecting_rectangle(
+            bounding(candidat),
+            bounding(clusteritem),
+        )
+
+    return utila.determine_cluster(todo, classifier, min_elements=min_elements)
+
+
+utila.intersecting_rectangle_cluster = intersecting_rectangle_cluster
+
+
+def intersecting_rectangle(first, second):
+    """\
+    >>> intersecting_rectangle((0, 45, 55, 100), (45, 0, 55, 100))
+    True
+    >>> intersecting_rectangle((0, 0, 50, 50), (55, 55, 100, 100))
+    False
+    >>> intersecting_rectangle((0, 0, 50, 50), (0, 0, 50, 50)) # identical
+    True
+    """
+    for first_line in rectangle_border(first):
+        for second_line in rectangle_border(second):
+            try:
+                if utila.intersecting_lines(first_line, second_line):
+                    return True
+            except utila.IndenticalLineError:
+                return True
+    return False
+
+
+def rectangle_border(rectangle):
+    """Run round trip on rectangle border.
+
+    >>> list(rectangle_border((0,0, 55, 50)))
+    [(0, 0, 55, 0), (55, 0, 55, 50), (0, 50, 55, 50), (0, 0, 0, 50)]
+
+    Hint: Left coordiante is always smaller equal than right coordiate
+    """
+    x0, y0, x1, y1 = rectangle
+    # left -> right
+    yield (x0, y0, x1, y0)
+    # right, top bottom
+    yield (x1, y0, x1, y1)
+    # right, right <- left
+    yield (x0, y1, x1, y1)
+    # left, bottom up
+    yield (x0, y0, x0, y1)

af027b4a688ec60bdc519105b348720a058b5ea2
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-12-19 02:20:31 +0100

    2.9.4 automatically generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v2.9.4
+
 ## v2.9.3
 
 ## v2.9.2
--- rawmaker/__init__.py
+++ rawmaker/__init__.py
@@ -16,7 +16,7 @@ import os
 
 import rawmaker.__patch__
 
-__version__ = '2.9.3'
+__version__ = '2.9.4'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 

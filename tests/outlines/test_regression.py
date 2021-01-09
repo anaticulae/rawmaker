ea883179d00ca169f7379da352fa717633a0cc78
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-01-09 10:07:00 +0100

    fix(outlines): extend outlines to support FitH

---
+++ tests/outlines/test_regression.py
@@ -0,0 +1,25 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import os
+
+import hardcore
+import serializeraw
+
+import tests
+
+
+def test_imagetext_outlines(testdir, monkeypatch):
+    """Regression test to load outlines `FitH` correctly."""
+    cmd = f'-i {hardcore.H000_IMAGETEXT_6_PDF} --outlines'
+    tests.run(cmd, monkeypatch=monkeypatch)
+    source = os.path.join(testdir.tmpdir, 'rawmaker__outlines_outlines.yaml')
+    assert os.path.exists(source)
+    loaded = serializeraw.load_toc(source)
+    assert len(loaded) == 6

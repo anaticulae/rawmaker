fcdba0935d9ee5180b923946773bb064b42f2cf5
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-01-15 19:47:03 +0100

    feat(optimize): add first optimizer to verify good rawmaker config
    to determine table correctly.

---
+++ optimize/linero_.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import os
+
+import power
+import serializeraw
+import utila
+
+import linero
+import linero.path
+
+CHAR_MARGIN = utila.ranged_exp(0.1, 50, steps=8)
+WORD_MARGIN = utila.ranged_exp(0.1, 80, steps=12)
+LINE_MARGIN = utila.ranged_exp(0.01, 2.0, steps=5)
+
+
+def score(result, parameter) -> float:  # pylint:disable=W0613,C0103
+    # scored = 2 * x * y
+    return result
+
+
+def run(path: str, char_margin: float, word_margin: float, line_margin: float):
+    cmd = (f'rawmaker -i {power.BACHELOR056_PDF} --pages=15 '
+           f'--char_margin={char_margin} --word_margin={word_margin} '
+           f'--line_margin={line_margin} '
+           '--text --line')
+
+    utila.run(cmd, cwd=path)
+    utila.run('linero', cwd=path)
+
+    path = linero.path.table(path)
+    loaded = serializeraw.load_tables(path)
+    utila.log(f'>>> {len(loaded)}')
+    return len(loaded)
+
+
+def present(result):
+    for item in result:
+        utila.log(item)
+
+
+if __name__ == "__main__":
+    TMPDIR = utila.tmpdir(linero.ROOT)
+    utila.log(TMPDIR)
+    utila.run(
+        f'optimo -i {os.path.abspath(__file__)} -VV -j=8',
+        cwd=TMPDIR,
+        live=True,
+    )

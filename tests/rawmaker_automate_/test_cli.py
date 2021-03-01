8ba8e0b9ce76e6106b1100dff6ac473a83f28f0f
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-03-01 17:43:37 +0100

    test(automate): add test to run rawmaker_automate

---
+++ tests/rawmaker_automate_/test_cli.py
@@ -0,0 +1,25 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import os
+
+import utila
+import utilatest
+
+import tests.rawmaker_automate_
+import tests.resources
+
+
+@utilatest.nightly
+def test_cli_automate(testdir, monkeypatch):
+    source = os.path.join(tests.resources.RESOURCES, 'increasing_fonts')
+    cmd = f'-i {source} -o {testdir.tmpdir} -n3'
+    assert os.path.exists(source)
+    tests.rawmaker_automate_.run(cmd, monkeypatch=monkeypatch)
+    assert len(utila.directory_list(testdir.tmpdir)) == 3

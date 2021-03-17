562b38f1e1c5087ebabce9b771e90f1f0a72bb37
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-03-17 11:42:34 +0100

    test(figure): add regression test to avoid false positive images

--- tests/figure/test_regression.py
+++ tests/figure/test_regression.py
@@ -7,6 +7,8 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import os
+
 import power
 import utilatest
 
@@ -21,3 +23,12 @@ def test_extract_figures_memory_error(testdir, monkeypatch, capsys):
     tests.run(f'-i {source}  --figures --pages=75:', monkeypatch=monkeypatch)
     stderr = capsys.readouterr().err
     assert 'could not render' in stderr, str(stderr)
+
+
+def test_nofigure_diss266_pagex(testdir, monkeypatch):
+    # TODO: INVESTIGATE WHERE DOE THESE RECTANGLES COME FROM!
+    source = power.DISS266_PDF
+    pages = '27,28,61'
+    tests.run(f'-i {source} --figures --pages={pages}', monkeypatch=monkeypatch)
+    # do not detect any figures on page 27, 28, 61
+    assert not os.path.exists('rawmaker__figures_figures')

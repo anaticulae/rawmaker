02d8c7460ec2895646317b993f9694c24ebfd297
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-07-13 17:06:03 +0200

    feat(linero): disable camelot file path check

--- linero/__patch__.py
+++ linero/__patch__.py
@@ -6,3 +6,21 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
+
+import camelot.handlers
+
+
+def __init__(self, filepath, pages="1", password=None):
+    self.filepath = filepath
+
+    if password is None:
+        self.password = ""
+    else:
+        self.password = password
+        if sys.version_info[0] < 3:
+            self.password = self.password.encode("ascii")
+    self.pages = self._get_pages(self.filepath, pages)
+
+
+# disable path check, we know what we do.
+camelot.handlers.PDFHandler.__init__ = __init__

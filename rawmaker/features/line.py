bf41ffa88efdccc05108bcc3f1ce4054125b5a60
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-08-05 10:47:20 +0200

    refactor(replace): replace with utila code

--- rawmaker/features/line.py
+++ rawmaker/features/line.py
@@ -26,7 +26,6 @@ import pdfminer.pdfdocument
 import serializeraw
 import utila
 
-import rawmaker.features.boxes
 import rawmaker.reader
 
 # maximal difference in y-component
@@ -52,11 +51,11 @@ def determine_lines(
     result = []
     for content, number in lines_:
         # left point is left above from right down point
-        content = [ensure_position(item) for item in content]
+        content = [utila.rectangle_ensure_bounding(item) for item in content]
         # top down, left right
         content = sorted(content, key=operator.itemgetter(1, 0))
         # merge lines which are divided by pdf printer
-        merged = merge_lines(content)
+        merged = utila.merge_lines(content)
         result.append(iamraw.PageContentLine(content=merged, page=number))
     return result
 
@@ -117,11 +116,11 @@ def lines(
         # remove very short lines/dots
         page = [item for item in page if not utila.isdot(item, max_length=5.0)]
         # ensure left, top, right, down bounding
-        page = [ensure_position(item) for item in page]
+        page = [utila.rectangle_ensure_bounding(item) for item in page]
         # sort item top down; left right
         page = sorted(page, key=operator.itemgetter(1, 0))
         # merges divided lines
-        page = merge_lines(page)
+        page = utila.merge_lines(page)
         # remove duplicated lines which mainly produces out of bad figure
         # extraction
         # TODO: ADD LINE DENSITY CHECK?
@@ -190,7 +189,7 @@ def accept_curve_as_line(curve: pdfminer.layout.LTCurve) -> bool:
     # more than two points in a row, check if point are on a line
     # [(437.04645, 259.38056), (437.04645, 293.26655), (437.04645, 269.60483999999997)]
     items = [(*first, *second) for first, second in zip(points[:-1], points[1:])] # yapf:disable
-    merged = merge_lines(items)
+    merged = utila.merge_lines(items)
     if len(merged) == 1:
         # all lines in a row
         return True
@@ -215,83 +214,6 @@ def figure_special_line(figure: pdfminer.layout.LTFigure) -> bool:
     middle = utila.roundme((figure.bbox[1] + figure.bbox[3]) / 2)
     figure.bbox = (figure.bbox[0], middle, figure.bbox[2], middle)
     return True
-
-
-def merge_lines(items, diff: float = 3.0):  # TODO: HOLY VALUE
-    """Some pdf printer prints long lines as a couple of short lines.
-    For analysis it is required, that these lines are merged to single
-    lines to work with correct length and positon.
-
-    This algorithm merges lines which are:
-     - connected in two points
-     - have the same raising.
-
-    As a requirement, the lines are sorted top down and left right.
-
-    >>> merge_lines([(328.18, 373.08, 329.68, 373.83),
-    ...              (329.68, 373.08, 416.03, 373.83),
-    ...              (416.02, 373.08, 416.77, 373.83),
-    ...              (416.77, 373.08, 502.39, 373.83),
-    ...              (502.40, 373.08, 504.65, 373.83),])
-    [(328.18, 373.08, 504.65, 373.83)]
-
-    >>> merge_lines([(257.58, 440.65 ,259.08 ,442.15),
-    ...              (259.08, 440.65 ,328.18 ,442.15),
-    ...              (328.18, 440.65 ,329.68 ,442.15),
-    ...              (329.68, 440.65 ,416.03 ,441.40),
-    ...              (416.02, 440.65 ,416.77 ,442.15),
-    ...              (416.77, 440.65 ,502.39 ,441.40),
-    ...              (502.40, 440.65 ,504.65 ,442.15)])
-    [(257.58, 440.65, 504.65, 442.15)]
-    """
-    if not items:
-        return []
-    result = [items[0]]
-    for item in items[1:]:
-        last_x1, last_y1 = result[-1][2], result[-1][3]
-        x0, y0 = item[0], item[1]
-        if not all((
-                utila.near(last_x1, x0, diff=diff),
-                utila.near(last_y1, y0, diff=diff),
-                utila.near(
-                    raising(item, diff),
-                    raising(result[-1], diff),
-                    diff=diff,
-                ),
-        )):
-            result.append(item)
-        else:
-            # unite
-            new = (result[-1][0], result[-1][1], item[2], item[3])
-            result.pop()
-            result.append(new)
-
-    # sort item top down; left right after merging
-    result = sorted(result, key=operator.itemgetter(1, 0))
-    return result
-
-
-def raising(item, diff=1.0) -> float:
-    """\
-    >>> raising((0, 0, 50, 50))
-    1.0
-    >>> raising((0, 0, 0, 100)) # inf raising
-    2147483647
-    """
-    xdiff = (item[2] - item[0])
-    ydiff = (item[3] - item[1])
-    if -diff <= ydiff <= diff:
-        return 0.0
-    if -diff <= xdiff <= diff:
-        return utila.INF
-    return xdiff / ydiff
-
-
-def ensure_position(item: tuple) -> tuple:
-    x0, y0, x1, y1 = item
-    x0, x1 = min([x0, x1]), max([x0, x1])
-    y0, y1 = min([y0, y1]), max([y0, y1])
-    return (x0, y0, x1, y1)
 
 
 def type_in_document(

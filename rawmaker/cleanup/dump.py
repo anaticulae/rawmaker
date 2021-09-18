ab7653c438dbc533ed58d201c8422ca2d98bc499
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-09-18 14:11:31 +0200

    refactor(cleanup): move ptn dumper to separate module

---
+++ rawmaker/cleanup/dump.py
@@ -0,0 +1,128 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import iamraw
+import serializeraw
+import serializeraw.fonts
+import texmex
+import utila
+
+import rawmaker.features.fonts
+import rawmaker.fonts.parser
+
+
+def dump_ptn(ptns: texmex.PageTextNavigators, fontstore: iamraw.FontStore):
+    document = iamraw.Document(dimension=ptns[0].pagesize)
+    textpositions = []
+    for page in ptns:
+        children, posis = create_page(page, fontstore)
+        content = iamraw.Page(
+            children=children,
+            page=page.page,
+            dimension=page.pagesize,
+        )
+        positions = iamraw.PageContentTextPosition(
+            page=page.page,
+            content=posis,
+        )
+        document.append(content)
+        textpositions.append(positions)
+    # write document
+    dumped_document = serializeraw.dump_document(document)
+    dumped_textpositions = serializeraw.dump_textpositions(textpositions)
+    fontstore, fontcontent = rawmaker.features.fonts.parse_fonts(document)
+    dumped_header = serializeraw.dump_font_header(fontstore)
+    dumped_content = serializeraw.dump_font_content(fontcontent)
+    return dumped_document, dumped_textpositions, dumped_header, dumped_content
+
+
+def create_page(page, fontstore: iamraw.FontStore) -> iamraw.Page:
+    if not page:
+        return [], {}
+    lines, positions = [], []
+    for item in page:
+        line = create_line(item, fontstore)
+        lines.append((item.line, line))
+        position = iamraw.TextPosition(
+            bounding=item.bounding,
+            mean=item.bounding_mean,
+        )
+        positions.append(position)
+    container, positions = merge_neighbors(lines, positions)
+    positions = dict(enumerate(positions))
+    return container, positions
+
+
+def merge_neighbors(lines, positions):
+    container, current = [lines[0][1]], lines[0][0]
+    textpositions = [positions[0]]
+    for ((number, line), texpos) in zip(lines[1:], positions[1:]):
+        if (number - current) == 1:
+            # merge
+            before = container[-1]
+            # add content
+            before.lines.extend(line.lines)
+            # update rectangle
+            before.box = utila.rectangle_max((before.box, line.box))
+            # merge textpositions
+            textpositions[-1] = iamraw.TextPosition(
+                bounding=tuple(before.box),
+                mean=textpositions[-1].mean,
+            )
+            # update last line id to merge further items
+            current = number
+        else:
+            # add new one
+            container.append(line)
+            textpositions.append(texpos)
+            # reset merger
+            current = 0
+    return container, textpositions
+
+
+def create_line(item, fontstore: iamraw.fontstore) -> iamraw.line:
+    rotation = item.style.rotation
+    if rotation:
+        line = iamraw.VerticalTextContainer(box=item.bounding)
+    else:
+        line = iamraw.TextContainer(box=item.bounding)
+    style = item.style.content
+    sizes = utila.flatten([
+        (item.end - item.start) * [item.size] for item in style
+    ])
+    rises = utila.flatten([
+        (item.end - item.start) * [item.rise] for item in style
+    ])
+    fonts = utila.flatten([
+        (item.end - item.start) *
+        [rawmaker.fonts.parser.font_toraw(fontstore[item.font])]
+        for item in style
+    ])
+    flags = utila.flatten([
+        (item.end - item.start) *
+        [serializeraw.fonts.toflag(fontstore[item.font].flags)]
+        for item in style
+    ])
+    chars = [
+        iamraw.Char(
+            value=value,
+            size=size,
+            rise=rise,
+            font=font,
+            flags=flag,
+        ) for value, size, rise, font, flag in zip(
+            item.text,
+            sizes,
+            rises,
+            fonts,
+            flags,
+        )
+    ]
+    line.append(iamraw.Line(chars=chars, box=item.bounding))
+    return line

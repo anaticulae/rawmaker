08c8325f41e44b867ba9c30718aef947babc93bb
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-11-04 17:46:15 +0100

    test(translate): translate 3 pages of bachelor037

---
+++ tests/translate/test_translate.py
@@ -0,0 +1,38 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import power
+import utila
+import utilatest
+
+import rawmaker.translate.lines
+import serializeraw
+import tests
+
+
+@utilatest.longrun
+def test_translate_lines(testdir, monkeypatch):
+    source = power.BACHELOR037_PDF
+    pages = '--pages=22,23,24'
+    # run rawmaker
+    tests.run(
+        f'-i {source} {pages} --text --fonts --images',
+        monkeypatch=monkeypatch,
+    )
+    utila.run(f'figureo -i {source} '
+              f'-i {testdir.tmpdir} -o {testdir.tmpdir} {pages}')
+    # do not cache load_documents, do not use tests.cleanup.run
+    utila.run('rawmaker_cleanup --backup '
+              f'-i {testdir.tmpdir} -o {testdir.tmpdir}')
+    ptn = serializeraw.ptn_frompath(testdir.tmpdir)
+    backup = serializeraw.ptn_frompath(testdir.tmpdir, backup=True)
+    assert ptn != backup, 'cached load_documents? check backup=False'
+    translated = rawmaker.translate.lines.translates(backup, ptn)
+    # changes on two pages, no change on page 22
+    assert len(translated) == 3

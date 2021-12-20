feb889d2fe774f12868da68a75fdef5e4bcba8d3
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-12-20 14:31:29 +0100

    feat(utils): add method to guess encoding

--- rawmaker/utils.py
+++ rawmaker/utils.py
@@ -9,8 +9,27 @@
 
 import contextlib
 
+import utila
+
 
 def resolve(reference):
     with contextlib.suppress(AttributeError):
         return reference.resolve()
     return reference
+
+
+ENCODINGS = utila.splititems('utf8 ascii cp1252')
+
+
+def guess_decoding(text: bytes) -> str:
+    r"""\
+    >>> guess_decoding(b'http://road.cc/measure-\x96-smart-street')
+    'http://road.cc/measure-–-smart-street'
+    """
+    for encoding in ENCODINGS:
+        try:
+            text = text.decode(encoding)
+        except UnicodeDecodeError:
+            continue
+        return text
+    return None

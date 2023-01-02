2a0ff15872f48b240d1b18bf4f5d3b48458af928
Helmut Konrad Schewe <helmutus@outlook.com>
2023-01-02 00:06:05 +0000

    chore(setup): use quick install

--- setup.py
+++ setup.py
@@ -8,58 +8,29 @@
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'rawmaker/__init__.py'), encoding='utf8') as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES = [
+    'letty',
+    'letty.quality',
+    'rawmaker',
+    'rawmaker.converter',
+    'rawmaker.features',
+    'rawmaker.fonts',
+    'rawmaker.images',
+    'rawmaker.miner',
+    'rawmaker.patch',
+    'rawmaker.text',
+    'spacestation',
+    'spacestation.features',
+]
+ENTRY_POINTS = dict(console_scripts=[
+    'letty = letty.cli:main',
+    'rawmaker = rawmaker.cli:main',
+    'rawmaker_automate = rawmaker.cli_automate:main',
+    'rawmaker_cleanup = cleanup.cli:main',
+    'spacestation = spacestation.cli:main',
+])
 
 if __name__ == "__main__":
-    setuptools.setup(
-        author='Helmut Konrad Schewe',
-        author_email='kiwi@derspanier.de',
-        description='Convert PDF to raw data',
-        install_requires=REQUIRES,
-        long_description=README,
-        name='rawmaker',
-        platforms='any',
-        url='https://rawmaker.dev.packages/checkitweg.de',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.8',
-        ],
-        packages=[
-            'letty',
-            'letty.quality',
-            'rawmaker',
-            'rawmaker.converter',
-            'rawmaker.features',
-            'rawmaker.fonts',
-            'rawmaker.images',
-            'rawmaker.miner',
-            'rawmaker.patch',
-            'rawmaker.text',
-            'spacestation',
-            'spacestation.features',
-        ],
-        entry_points={
-            'console_scripts': [
-                'letty = letty.cli:main',
-                'rawmaker = rawmaker.cli:main',
-                'rawmaker_automate = rawmaker.cli_automate:main',
-                'rawmaker_cleanup = cleanup.cli:main',
-                'spacestation = spacestation.cli:main',
-            ],
-        },
-    )
+    utila.install(__file__)
